fbeaa4c35f706f85f1eb0a4155d9ee317fdf4391
barbara <barbs.boechat@gmail.com>
2019-04-02 09:30:29 -0300

    cliente leva erro no get

--- cliente.rb
+++ cliente.rb
@@ -1,9 +1,27 @@
 require 'socket'
+require 'fileutils'
 
-socket = TCPSocket.new 'localhost', 8000
+#socket = TCPSocket.new 'localhost', 8000
 
-while true
-  msg = $stdin.gets.chomp # $stdin is an object that's always present and manages user input from the console
-  socket.puts( msg ) # take what you entered and send it via TCP to the socket file
-  puts socket.gets.chomp # wait for more content to appear in the socket and print it to the console
-end
+host = 'www.tutorialspoint.com'     # The web server
+port = 80                       # Default HTTP port
+path = '/index.html'                 # The file we want
+
+# This is the HTTP request we send to fetch a file
+request = "GET #{path} HTTP/1.0\r\n\r\n"
+#addr = Socket.getaddrinfo(host, "http", nil, :STREAM)
+#puts "oie = #{addr.at(2)}"
+socket = TCPSocket.new host, port
+
+socket.print(request)               # Send request
+response = socket.read              # Read complete response
+# Split response at first blank line into headers and body
+headers,body = response.split("\r\n\r\n", 2)
+print body
+
+dirname = "public_html"
+file_name = 'index.html'
+FileUtils.mkdir_p(dirname) unless Dir.exists?(dirname)
+f = File.open(File.join(Dir.pwd, dirname, file_name),"w+")
+f.write(body)
+f.close

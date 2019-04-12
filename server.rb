870a50e4854dc0ca1217d5347f5b9dafe2fa09b4
barbara <barbs.boechat@gmail.com>
2019-04-11 21:42:01 -0300

    ainda querendo chorar

--- server.rb
+++ server.rb
@@ -2,9 +2,9 @@ require 'socket'
 server = TCPServer.new 8000
 
 PAGES = {
-  "/" => "Hi, welcome to the home page!",
-  "/about" => "About us: we are http hackers",
-  "/news" => "We haven't made much news yet with this server, but stay tuned"
+  "/" => "index.html",
+  "/about" => "about.html",
+  "/news" => "news.html"
 }
 
 PAGE_NOT_FOUND = "Sorry, there's nothing here."
@@ -15,18 +15,23 @@ loop do
   while (line = session.gets) && (line.chomp.length > 0)
     request << line.chomp
   end
+#  puts  "o que eu quero é #{request}"
   puts "finished reading"
 
   http_method, path, protocol = request[0].split(' ') # there are three parts to the request line
+  puts "#{http_method} #{path} #{protocol}"
 
   if PAGES.keys.include? path
     status = "200 OK"
-    response_body = PAGES[path]
+    dirname = "public"
+    f = File.open(PAGES[path],"r")
+    response_body = f.read()
+    f.close
   else
     status = "404 Not Found"
     response_body = PAGE_NOT_FOUND
   end
-
+  puts response_body
   session.puts <<-HEREDOC
 HTTP/1.1 #{status}
 
